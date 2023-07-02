431fc9ae4f3a102a9832719e320b45e0d0c31281
Wenzhuo Zhao <zslyvain@gmail.com>
2023-07-02 02:00:46 +0200

    fix(infra): user model
    add user model and migrations

---
+++ food_order_system/infra/db/model/user_dto.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+from sqlalchemy import Boolean, DateTime, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from food_order_system.infra.db.model.base import Base
+
+
+class UserDTO(Base):
+    __tablename__ = "user"
+    name: Mapped[str] = mapped_column(String(255), nullable=False)
+    phone_number: Mapped[str] = mapped_column(
+        String(255),
+        nullable=False,
+        primary_key=True,
+    )
+    is_verified: Mapped[Boolean] = mapped_column(Boolean, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        nullable=False,
+        default=datetime.now(),
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        nullable=False,
+        default=datetime.now(),
+        onupdate=datetime.now(),
+    )
