7da15760d035935db3ae65a9a4e5690dfc2596d8
Wenzhuo Zhao <zslyvain@gmail.com>
2023-07-02 02:01:20 +0200

    fix(infra): base repository
    add base repository and user repository

---
+++ food_order_system/infra/db/repository/base.py
@@ -0,0 +1,77 @@
+from typing import Any, Generic, Type, TypeVar
+
+from sqlalchemy.engine.result import Result
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+
+from food_order_system.infra.db.model.base import Base
+
+T = TypeVar("T", bound=Base)
+TEntity = TypeVar("TEntity", bound=Base)
+
+
+class BaseRepository(Generic[T]):
+    def __init__(self, model: Type[T], session: AsyncSession):
+        self.model = model
+        self.session = session
+
+    async def _query(self, *args: Any, **kwargs: Any) -> Result[T]:
+        filters = [getattr(self.model, k) == v for k, v in kwargs.items()]
+        filters.extend(iter(args))
+        stmt = select(self.model).filter(*filters)
+        return await self.session.execute(stmt)
+
+    async def get(self, *args: Any, **kwargs: Any) -> T | None:
+        result = await self._query(*args, **kwargs)
+        return result.scalars().one_or_none()
+
+    async def get_many(self, *args: Any, **kwargs: Any) -> list[T]:
+        result = await self._query(*args, **kwargs)
+        return result.scalars().all()
+
+    async def create(self, obj_in: dict[str, Any] | T) -> T:
+        if isinstance(obj_in, dict):
+            return await self._create_from_dict(obj_in)
+        if isinstance(obj_in, self.model):
+            return await self._create_from_model(obj_in)
+        raise TypeError(
+            f"obj_in must be of type {self.model} or dict, not {type(obj_in)}",
+        )
+
+    async def _create_from_model(self, obj_in: T) -> T:
+        return await add_and_commit(self.session, obj_in)
+
+    async def _create_from_dict(self, obj_in: dict[str, Any]) -> T:
+        return await add_and_commit(self.session, self.model(**obj_in))
+
+    async def get_by_id(self, id: int) -> T | None:
+        return await self.get(id=id)
+
+    async def update(self, obj_in: T) -> T:
+        await self.session.commit()
+        return obj_in
+
+    async def delete(self, obj_in: T) -> None:
+        await self.session.delete(obj_in)  # type: ignore
+        await self.session.commit()
+
+    async def delete_many(self, objs_in: list[T]) -> None:
+        for obj in objs_in:
+            await self.session.delete(obj)  # type: ignore
+        await self.session.commit()
+
+
+class RepositoryException(Exception):
+    pass
+
+
+async def add_and_commit(session: AsyncSession, obj: T) -> T:
+    try:
+        session.add(obj)  # type: ignore
+        await session.commit()
+    except Exception as e:
+        await session.rollback()
+        raise RepositoryException(
+            f"Error while adding {obj} to session: {str(e)}",
+        ) from e
+    return obj
---
+++ food_order_system/infra/db/repository/user.py
@@ -0,0 +1,6 @@
+from food_order_system.infra.db.model.user_dto import UserDTO
+from food_order_system.infra.db.repository.base import BaseRepository
+
+
+class UserRepository(BaseRepository[UserDTO]):
+    pass
