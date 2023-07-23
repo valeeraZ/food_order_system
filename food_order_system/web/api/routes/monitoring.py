a57d6a94578c3c5684644b1b9e300c13e277c8e1
Wenzhuo Zhao <zslyvain@gmail.com>
2023-07-24 00:21:47 +0200

    fix(routes): monitoring use async def

--- food_order_system/web/api/routes/monitoring.py
+++ food_order_system/web/api/routes/monitoring.py
@@ -1,32 +1,31 @@
 from fastapi import APIRouter, Depends, Response, status
 from sqlalchemy import text
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from food_order_system.infra.db.dependencies import get_db_session
 
 router = APIRouter()
 
 
-def is_database_online(session: Session) -> bool:
+async def is_database_online(session: AsyncSession) -> bool:
     try:
-        session.execute(text("SELECT 1"))
+        await session.execute(text("SELECT 1"))
         return True
     except Exception:
         return False
 
 
-def is_facebook_online():
-    return {"database": "online"}
-
-
 @router.get("/health", status_code=status.HTTP_200_OK, tags=["monitoring"])
-def health_check(response: Response, session: Session = Depends(get_db_session)):
+async def health_check(
+    response: Response,
+    session: AsyncSession = Depends(get_db_session),
+):
     """
     Checks the health of a project.
 
     It returns 200 if the project is healthy.
     """
-    if is_database_online(session):
+    if await is_database_online(session):
         response.status_code = status.HTTP_200_OK
         return {"database": "online"}
     response.status_code = status.HTTP_503_SERVICE_UNAVAILABLE
