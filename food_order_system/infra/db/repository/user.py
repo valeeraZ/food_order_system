c35e930ae175c546a83fbd80b0913081997b61f8
Wenzhuo Zhao <zslyvain@gmail.com>
2023-07-31 01:36:24 +0200

    refactor(infra): user: fix DI for get repo instance

--- food_order_system/infra/db/repository/user.py
+++ food_order_system/infra/db/repository/user.py
@@ -1,6 +1,16 @@
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from food_order_system.infra.db.dependencies import get_db_session
 from food_order_system.infra.db.model.user_dto import UserDTO
 from food_order_system.infra.db.repository.base import BaseRepository
 
 
 class UserRepository(BaseRepository[UserDTO]):
     pass
+
+
+def get_user_repository(
+    session: AsyncSession = Depends(get_db_session),
+) -> UserRepository:
+    return UserRepository(UserDTO, session)
